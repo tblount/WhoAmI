de436671c2fa5c4d1b610a66fb47b258154e7c7b
T.Blount <tblountcs@gmail.com>
2021-02-17 16:37:24 -0600

    Added Splash App, still working

--- src/com/games/whoami/client/Play.java
+++ src/com/games/whoami/client/Play.java
@@ -1,13 +1,24 @@
 package com.games.whoami.client;
 
 import com.apps.util.Prompter;
+import com.apps.util.SplashApp;
 import com.games.whoami.controller.Game;
 
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Play {
+public class Play implements SplashApp {
+
+    @Override
+    public void start() {
+        Game game = new Game();
+        game.initialize();
+    }
     public static void main(String[] args) {
+
+        Play app = new Play();
+        app.welcome("images/logo.jpg", "images/credits.png");
+        app.start();
         try {
             Game game = new Game();
             Prompter prompter = new Prompter(new Scanner(System.in));
