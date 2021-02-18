4ee935ca39eab009d5e912cf955b0f3793e873d8
asylbek09@gmail.com <Ronaldo76521826!>
2021-02-18 10:30:28 -0500

    fixed run.com

--- run.cmd
+++ run.cmd
@@ -1,1 +1,1 @@
-java -splash:images/team.jpg -classpath out\production\WhoAmI;lib\app-utils-1.0.2.jar com.games.whoami.client.Play
+java -splash:images/team.png -classpath out\production\WhoAmI;lib\app-utils-1.0.2.jar com.games.whoami.client.Play
